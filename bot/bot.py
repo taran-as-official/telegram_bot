f85962e75ad9a734953c6f944e852f13099d1ac9
Таран Артур Сергеевич <taran_as@corp.tander.ru>
2021-06-25 15:07:29 +0300

    Пытаемся победить синхронность при нажатии дострочного ответа

--- bot/bot.py
+++ bot/bot.py
@@ -54,8 +54,8 @@ async def process_callback_www_early_answer(callback_query: types.CallbackQuery)
 @dp.callback_query_handler(lambda c: c.data == 'www_give_minute')
 async def process_callback_www_give_minute(callback_query: types.CallbackQuery):
 
-    asyncio.ensure_future(www_game.give_minute(callback_query.from_user.id,callback_query.message.message_id))
-
+    #asyncio.ensure_future(www_game.give_minute(callback_query.from_user.id,callback_query.message.message_id))
+    await www_game.give_minute(callback_query.from_user.id,callback_query.message.message_id)
 
 
 
