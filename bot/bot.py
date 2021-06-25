ba18b815963a0f74eec5868cd8dd9ea090a2c427
Таран Артур Сергеевич <taran_as@corp.tander.ru>
2021-06-25 15:28:05 +0300

    Тестирование циклов событий

--- bot/bot.py
+++ bot/bot.py
@@ -54,8 +54,8 @@ async def process_callback_www_early_answer(callback_query: types.CallbackQuery)
 @dp.callback_query_handler(lambda c: c.data == 'www_give_minute')
 async def process_callback_www_give_minute(callback_query: types.CallbackQuery):
     #если будут какие то зацикленные функции, которые нужно будет остановить, то делать через такую конструкцию
-    #asyncio.ensure_future(www_game.give_minute(callback_query.from_user.id,callback_query.message.message_id))
-    await www_game.give_minute(callback_query.from_user.id, callback_query.message.message_id)
+    asyncio.ensure_future(www_game.give_minute(callback_query.from_user.id,callback_query.message.message_id))
+    #await www_game.give_minute(callback_query.from_user.id, callback_query.message.message_id)
 
 
 @dp.callback_query_handler(lambda c: c.data == 'www_next_question')
@@ -125,7 +125,7 @@ def main():
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        loop=asyncio.get_event_loop().run_forever(),
+        #loop=asyncio.get_event_loop().run_forever(),
         skip_updates=True,
         on_startup=on_startup,
         host=WEBAPP_HOST,
